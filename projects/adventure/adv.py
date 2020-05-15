354a8e5c41cd2fd779d850f85324d0713a3d5da8
Kumar Veeravel <mvkumar14@gmail.com>
2020-05-15 00:33:56 -0700

    started work on adv.py

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,9 +1,24 @@
+import time
 from room import Room
 from player import Player
 from world import World
 
 import random
 from ast import literal_eval
+
+
+def inv_direction(direction):
+    # returns n s opposites and e w opposites
+    if d == 'n':
+        return 's'
+    elif d == 's':
+        return 'n'
+    elif d == 'w':
+        return 'e'
+    elif d == 'e':
+        return 'w'
+    else:
+        print('error invalid direction')
 
 # Load world
 world = World()
@@ -29,7 +44,62 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# things to do in search loop
+# move
+# I have to add directions to traversal path
+# get id
+# update old room
+# add current room (if not added yet) to the
+# dictionary of rooms
+total_rooms = len(world.rooms)
+print("total rooms:",total_rooms)
 
+all_visited  = []
+my_room_graph = {}
+while len(all_visited) < 500:
+    rmid = player.current_room.id
+    rmexits = player.current_room.get_exits()
+
+    # I have to change rmexits into dictionary format
+    # and have all the keys be ? if its a brand new room
+
+    # move the starting to before the loop starts. 
+    if my_room_graph.__contains__(rmid):
+        pass
+    else:
+        my_room_graph[rmid] = rmexits
+    
+    print(my_room_graph)
+    
+    if rmid in all_visited:
+        pass
+    else:
+        all_visited.append(rmid)
+
+    # choose a direction to travel in:
+    random.sample()
+
+    player.travel(direction)
+    traversal_path.append(direction)
+    new_rmid = player.current_room.id
+    my_room_graph[rmid][direction] = new_rmid
+    if my_room_graph.__contains__(new_rmid):    
+        my_room_graph[new_rmid][inv_direction(direction)] = rmid
+    else:
+        my_room_graph[new_rmid] = player.current_room.get_exits()
+        my_room_graph[new_rmid][inv_direction(direction)] = rmid
+    time.sleep(0.5)
+
+
+
+
+#`player.current_room.id`, `player.current_room.get_exits()` and `player.travel(direction)
+
+
+
+# extension
+# Keep a stack of "rooms with ?s" + a dictionary to get from here to that previous room
+# so as you travel you "leave behind" a trail.
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +121,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
